eccf14c45cb00fd8c2c15911fe5a25c15fdc6e50
zjhou <z@zjh.im>
2021-03-24 23:31:12 +0800

    feat: runtime transform

--- src/config/rollup.config.js
+++ src/config/rollup.config.js
@@ -1,23 +1,22 @@
-const path = require('path');
-const babel = require('rollup-plugin-babel');
-const resolve = require('rollup-plugin-node-resolve');
-const commonjs = require('rollup-plugin-commonjs');
-const postcss = require('rollup-plugin-postcss');
-const peerDepsExternal = require('rollup-plugin-peer-deps-external');
-const json = require('@rollup/plugin-json');
-const { terser } = require('rollup-plugin-terser');
+const path = require("path");
+const babel = require("rollup-plugin-babel");
+const resolve = require("rollup-plugin-node-resolve");
+const commonjs = require("rollup-plugin-commonjs");
+const postcss = require("rollup-plugin-postcss");
+const peerDepsExternal = require("rollup-plugin-peer-deps-external");
+const json = require("@rollup/plugin-json");
+const { terser } = require("rollup-plugin-terser");
 
-const svgr = require('@svgr/rollup').default;
+const svgr = require("@svgr/rollup").default;
 
-const paths = require('../paths');
+const paths = require("../paths");
 
 const createRollupConfig = (opts = {}) => {
-
   const {
     input = paths.appIndexPath,
     outputPath = paths.distPath,
-    outputName = 'index.js',
-    outputFormat = 'es',
+    outputName = "index.js",
+    outputFormat = "es",
     packageJsonPath = paths.pkgJSONPath,
     production = true,
   } = opts;
@@ -29,46 +28,47 @@ const createRollupConfig = (opts = {}) => {
       sourcemap: !production,
       file: path.join(outputPath, outputName),
       format: outputFormat,
-      globals: { react: 'React' },
+      globals: { react: "React" },
     },
 
     watch: {
-      exclude: 'node_modules/**'
+      exclude: "node_modules/**",
     },
 
     plugins: [
       peerDepsExternal({
-        packageJsonPath
+        packageJsonPath,
       }),
       postcss({
         extract: false,
         modules: true,
-        use: ['sass'],
+        use: ["sass"],
       }),
       resolve(),
       svgr(),
       json(),
       babel({
-        exclude: 'node_modules/**',
-        presets: ['@babel/preset-env', '@babel/preset-react'],
+        exclude: "node_modules/**",
+        presets: ["@babel/preset-env", "@babel/preset-react"],
         plugins: [
           "@babel/plugin-syntax-export-default-from",
-          "@babel/plugin-proposal-class-properties"
-        ]
+          "@babel/plugin-proposal-class-properties",
+          "@babel/plugin-transform-runtime",
+        ],
       }),
       commonjs(),
       terser(),
     ],
     external: [
-      'react',
-      'react-dom',
-      'rxjs',
-      'rxjs/operators',
-      'immer',
-      'classnames',
-      'prop-types'
+      "react",
+      "react-dom",
+      "rxjs",
+      "rxjs/operators",
+      "immer",
+      "classnames",
+      "prop-types",
     ],
   };
-}
+};
 
 module.exports = createRollupConfig;
